a1887bbf0ede3b84a1d8267f23c6e56b2bfc98d5
Iti Bansal <itibansal.mca19.du@gmail.com>
2021-06-25 17:42:53 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
+
+const App = () => {
+    return(
+        <Router>
+            <Switch>   
+            <Route path = "/" exact component = {Join} />
+            <Route path = "/chat" exact component = {Chat} />
+            </Switch>
+        </Router>
+    )
+    
+}
+
+export default App
